c9fed4aaa9d3ddb658c9f838bec276a7f1994024
nickcrafford <nickcrafford@gmail.com>
2016-10-22 21:21:02 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+var express = require("express");
+var nunjucks = require("nunjucks");
+var request = require("request-json");
+var pusher = require("pusher");
+var bodyParser = require("body-parser");
+var uuid = require("node-uuid");
+
+var rawBodySaver = function (req, res, buf, encoding) {
+  if (buf && buf.length) {
+    req.rawBody = buf.toString(encoding || "utf8");
+  }
+}
+
+var till_url = process.env.TILL_URL;
+var pusher_url = process.env.PUSHER_URL;
+
+var pusher_token = pusher_url.split("http://")[1].split(":")[0];
+var till_base_url =till_url.split("addon")[0];
+var till_send_path = "addon" + till_url.split("addon")[1];
+
+var client = request.createClient(till_base_url);
+var pclient = pusher.forURL(pusher_url);
+
+var app = express();
+app.use(bodyParser.json({verify: rawBodySaver}));
+app.use(bodyParser.urlencoded({verify: rawBodySaver, extended: true}));
+app.use(bodyParser.raw({verify: rawBodySaver, type: function () { return true }}));
+nunjucks.configure("templates", {express: app});
+
+// Render UI
+app.get("/", function(req, res) {
+  var str_uuid = uuid.v1()+"";
+  res.render("index.html", {"uuid": str_uuid, "pusher_token": pusher_token});
+});
+
+// Send question over SMS
+app.post("/send", function(req, res) {
+  var data = {
+    "phone": [req.body.phone_number],
+    "questions" : [{
+      "text": "Favorite color?",
+      "tag": "favorite_color",
+      "responses": ["Red", "Green", "Yellow"],
+      "webhook": req.body.webhook_url + "?uuid="+req.body.uuid
+    }]
+  };
+
+  client.post(till_send_path, data, function(err, res, body) {
+    return console.log(res.statusCode);
+  });  
+
+  res.json({"status": "success"});
+});
+
+// Webhook for results
+app.post("/results", function(req, res) {
+  pclient.trigger(req.query.uuid, "result", req.body);
+  res.json({"status": "success"});
+});
+
+// Start server
+var server = app.listen(process.env.PORT || 5002, function () {
+  console.log("App now running on port", server.address().port);
+});
